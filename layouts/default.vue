545b6a650d793bff576129e7429fbb3aa1605b6c
Valentin <sosnov@slava.co.uk>
2021-11-23 20:58:47 +0300

    layouts FIX

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="layout_div">
+    <header-vue></header-vue>
+    <Nuxt />
+    <footer-vue></footer-vue>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "default"
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,19 @@
-<!--У меня возникают проблемы с Layout/default.vue,-->
-<!--я не понимаю как правильно прикрепить футер и хидер, -->
-<!--а так же правильно настроить роутинг между компонентами, -->
-<!--в документации перекопался, но при компиляции, default.vue в-->
-<!--.nuxt все равно перекампиливает и сбивает хидер и футер.-->
-<!--надеюсь на вашу помощь Сенсей)-->
-
-<!--Что бы посмотреть страничку мне приходиться закомментировать один-->
-<!--компонент и раскомментировать другой-->
-
 <template>
-  <div>
-    <header-vue></header-vue>
-    <Nuxt/>
-    <!--    <main-page></main-page>-->
-    <!--    <about-page></about-page>-->
-    <!--    <catalog></catalog>-->
-    <!--    <products></products>-->
-    <!--    <product-item></product-item>-->
-    <!--    <basket></basket>-->
-    <personal></personal>
-    <footer-vue></footer-vue>
-  </div>
+  <section class="container mx-auto">
+    <div>
+      <img src="~/assets/images/main-bg.jpg" alt="main-bg">
+    </div>
+  </section>
 </template>
 
 <script>
-import HeaderVue from "../components/headerVue";
-import FooterVue from "../components/footerVue";
-import Main from "./main";
-import About from "./about";
-import Products from "./products";
-import Catalog from "./catalog";
-import ProductItem from "./productItem";
-import Basket from "./basket";
-import Personal from "./personal";
-
 export default {
-  components: {Personal, Basket, ProductItem, Catalog, Products, About, Main, FooterVue, HeaderVue}
+  name: "main"
 }
 </script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+</style>
